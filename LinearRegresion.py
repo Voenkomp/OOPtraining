26e418362e250498d4ee69e089618dd4421b8180
Voenkomp <remesey97@gmail.com>
2024-10-30 17:30:54 +1000

    add LinearRegression.py

---
+++ LinearRegresion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn.datasets import make_regression
+
+X, y = make_regression(
+    n_samples=1000, n_features=14, n_informative=10, noise=15, random_state=42
+)
+X = pd.DataFrame(X)
+y = pd.Series(y)
+
+X.columns = [f"col_{col}" for col in X.columns]
+print(X.columns)

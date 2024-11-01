00bb7761161888f8649027cdd6ec1a19ce112dbe
Voenkomp <remesey97@gmail.com>
2024-11-01 17:27:17 +1000

    ''

--- LinearRegresion.py
+++ LinearRegresion.py
@@ -9,3 +9,19 @@ y = pd.Series(y)
 
 X.columns = [f"col_{col}" for col in X.columns]
 print(X.columns)
+
+
+class MyLineReg:
+    def __init__(self, n_iter, learning_rate):
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+
+    def __str__(self):
+        return (
+            f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
+        )
+
+
+object = MyLineReg(100, 0.5)
+
+print(object)

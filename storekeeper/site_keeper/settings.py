f26735131d244271dab933f2c40fbd9b7dcbd295
Voenkomp <remesey97@gmail.com>
2024-12-06 17:08:49 +1000

    connect db and make 2 models (Question and Choice)

--- storekeeper/site_keeper/settings.py
+++ storekeeper/site_keeper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "site_keeper.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "storedb",
+        "USER": "admin",
+        "PASSWORD": "doyouwannaenter",
+        "HOST": "srvbase",
+        "PORT": "32769",
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "UTC+10"
 
 USE_I18N = True
 

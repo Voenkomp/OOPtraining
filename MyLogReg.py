e07d4f193c7394527760d043ccf8ab0938e1d1f4
Voenkomp <remesey97@gmail.com>
2025-02-27 18:18:22 +1000

    implementation accuracy, precision, recall, f1, roc_auc(not finish) metrics

--- MyLogReg.py
+++ MyLogReg.py
@@ -14,17 +14,42 @@ X.columns = [f"col_{col}" for col in X.columns]
 
 
 class MyLogReg:
-    def __init__(self, n_iter=10, learning_rate=0.1):
+    def __init__(
+        self, n_iter: int = 10, learning_rate: float = 0.1, metric: str = None
+    ):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = None
+        self.metric = metric
 
     def __str__(self):
         return (
             f"MyLogReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
         )
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, verbose=False):
+    @staticmethod
+    def accuracy(y: pd.Series, y_hat: pd.Series):
+        return np.sum(np.where(y == y_hat, 1, 0)) / len(y)
+
+    @staticmethod
+    def precision(y: pd.Series, y_hat: pd.Series):
+        last_metric = np.sum(np.where((y == 1) & (y_hat == 1), 1, 0)) / (
+            np.sum(
+                np.where(((y == 1) & (y_hat == 1)) | ((y == 0) & (y_hat == 1)), 1, 0)
+            )
+        )
+        return last_metric
+
+    @staticmethod
+    def recall(y: pd.Series, y_hat: pd.Series):
+        last_metric = np.sum(np.where((y == 1) & (y_hat == 1), 1, 0)) / (
+            np.sum(
+                np.where(((y == 1) & (y_hat == 1)) | ((y == 1) & (y_hat == 0)), 1, 0)
+            )
+        )
+        return last_metric
+
+    def fit(self, X: pd.DataFrame, y: pd.Series, verbose: bool = False):
 
         X.insert(
             loc=0, column="ones", value=1
@@ -43,6 +68,8 @@ class MyLogReg:
             )
 
             if verbose and (i == 1 or i % verbose) == 0:
+                if self.metric:
+                    print(f"{i} | loss: {Logloss} | {self.metric}: ")
                 print(f"{i} | loss: {Logloss}")
 
             grad = 1 / len(y) * np.dot((y_hat - y), X)  # расчет градиента
@@ -50,6 +77,10 @@ class MyLogReg:
 
     def get_coef(self):
         return self.weights[1:]
+
+    def get_best_score(self):
+        last_metric = getattr(self, self.metric)(y, self.predict(X))
+        return last_metric
 
     """возращает вероятности"""
 
@@ -64,11 +95,12 @@ class MyLogReg:
         return np.where(self.predict_proba(X) > 0.5, 1, 0)
 
 
-object1 = MyLogReg(n_iter=50, learning_rate=0.1)
+object1 = MyLogReg(n_iter=50, learning_rate=0.1, metric="precision")
 
 object1.fit(X, y)
 
 # print(np.mean(object1.get_coef()))
 # print(object1.predict_proba(X))
-print(np.mean(object1.predict_proba(X)))
-print(sum(object1.predict(X)))
+# print(np.mean(object1.predict_proba(X)))
+# print(sum(object1.predict(X)))
+print(object1.get_best_score())

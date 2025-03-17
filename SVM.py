1ec77e1fb3abeed146523358c89491289bd38dd1
Voenkomp <remesey97@gmail.com>
2025-03-17 23:40:35 +1000

    edit SVM.py

--- SVM.py
+++ SVM.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 
 from sklearn.datasets import make_classification
 
@@ -10,11 +11,21 @@ X, y = pd.DataFrame(X), pd.Series(y)
 
 
 class MySVM:
-    def __init__(self, n_iter: int = 10, learning_rate: float = 0.001):
+    def __init__(
+        self,
+        n_iter: int = 10,
+        learning_rate: float = 0.001,
+        C: int = 1,
+        sgd_sample=None,
+        random_state: int = 42,
+    ):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = None
         self.b = None
+        self.C = C
+        self.sgd_sample = sgd_sample
+        self.random_state = random_state
 
     def __repr__(self):
         return f"MySVM class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
@@ -25,29 +36,40 @@ class MySVM:
         self.weights = np.ones(X.shape[1])
         self.b = 1
 
+        random.seed(self.random_state)
+
+        if isinstance(self.sgd_sample, float) and 0 < self.sgd_sample <= 1:
+            self.sgd_sample = int(self.sgd_sample * X.shape[0])
+
         for i in range(self.n_iter):
+
+            if self.sgd_sample:
+                sample_rows_idx = random.sample(range(X.shape[0], self.sgd_sample))
+
             """Calculate LOSS FUNCTION"""
             if verbose and ((i + 1) % verbose == 0 or i == 0):
                 hinge_loss = 0
                 for idx in range(X.shape[0]):
                     X_i, y_i = X.iloc[idx], y.iloc[idx]
                     hinge_loss += max(0, 1 - y_i * (self.weights @ X_i + self.b))
-                LOSS_F = np.sum(self.weights**2) + hinge_loss / len(y)
+                LOSS_F = np.sum(self.weights**2) + (self.C * hinge_loss) / len(y)
                 print(f"{i + 1} | loss: {LOSS_F}")
             """End calculate"""
 
-            for idx in range(X.shape[0]):
-                X_i, y_i = X.iloc[idx], y.iloc[idx]
+            if self.sgd_sample:
+                sample_rows_idx = random.sample(range(X.shape[0], self.sgd_sample))
+                for idx in sample_rows_idx:
+                    X_i, y_i = X.iloc[idx], y.iloc[idx]
 
-                if y_i * (self.weights @ X_i + self.b) >= 1:
-                    gradient_w = 2 * self.weights
-                    gradient_b = 0
-                else:
-                    gradient_w = 2 * self.weights - y_i * X_i
-                    gradient_b = -y_i
+                    if y_i * (self.weights @ X_i + self.b) >= 1:
+                        gradient_w = 2 * self.weights
+                        gradient_b = 0
+                    else:
+                        gradient_w = 2 * self.weights - self.C * y_i * X_i
+                        gradient_b = -(self.C * y_i)
 
-                self.weights -= self.learning_rate * gradient_w
-                self.b -= self.learning_rate * gradient_b
+                    self.weights -= self.learning_rate * gradient_w
+                    self.b -= self.learning_rate * gradient_b
 
     def get_coef(self):
         return self.weights, self.b

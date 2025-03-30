48ad17e539e0306af6bd901805b6abda3b628ea2
Voenkomp <remesey97@gmail.com>
2025-03-30 23:46:41 +1000

    edit MyKNNClf.py, add weightkNN

--- MyKNNClf.py
+++ MyKNNClf.py
@@ -9,12 +9,13 @@ X.columns = [f"col_{col}" for col in X.columns]
 
 
 class MyKNNClf:
-    def __init__(self, k: int = 3, metric: str = "euclidean"):
+    def __init__(self, k: int = 3, metric: str = "euclidean", weight: str = "uniform"):
         self.k = k
         self.train_size = None
         self.X = None
         self.y = None
         self.metric = metric
+        self.weight = weight
 
     def __repr__(self):
         return f"MyKNNClf class: k={self.k}"
@@ -39,24 +40,31 @@ class MyKNNClf:
         denominator = np.sqrt(np.sum((row**2))) * np.sqrt((self.X_train**2).sum(axis=1))
         return 1 - (numerator / denominator)
 
-    def euclid_mean_predict(self, row: pd.Series) -> int:
-        mean = self.euclid_mean(row)
-        return 1 if mean >= 0.5 else 0
+    def get_metrics_predict(self, row: pd.Series) -> int:
+        class_index = self.get_metrics_mean(row)
+        if self.weight == "rank":
+            class_index = class_index.reset_index(drop=True)
+            class_index.index += 1
+            q_class1 = (1 / pd.Series(class_index[class_index == 1].index)).sum()
+            q_class0 = (1 / pd.Series(class_index[class_index == 0].index)).sum()
+            denominator = 1 / pd.Series(range(1, class_index.shape[0])).sum()
+            return 1 if q_class1 / denominator > q_class0 / denominator else 0
+        return 1 if class_index.mean() >= 0.5 else 0
 
     def predict(self, X_test: pd.DataFrame):
-        return X_test.apply(self.euclid_mean_predict, axis=1)
+        return X_test.apply(self.get_metrics_predict, axis=1)
 
-    def euclid_mean(self, row: pd.Series) -> float:
+    def get_metrics_mean(self, row: pd.Series) -> pd.Series:
         dist_min_ind = (
             getattr(self, "_" + self.metric + "_distance")(row)
             .sort_values()
             .head(self.k)
             .index
         )
-        return self.y_train[dist_min_ind].mean()
+        return self.y_train[dist_min_ind]  # убрал .mean() отсюда
 
     def predict_proba(self, X_test: pd.DataFrame):
-        return X_test.apply(self.euclid_mean, axis=1)
+        return X_test.apply(self.get_metrics_mean, axis=1)
 
 
 obj1 = MyKNNClf(1)
--- scratch.py
+++ scratch.py
@@ -1,25 +1,11 @@
 import pandas as pd
 import numpy as np
 
-X = pd.DataFrame({"col1": [1, 2, 1, 4, 5], "col2": [6, 7, 8, 6, 10]})
-X_test = pd.DataFrame({"col1": [1, 7, 3, 4, 5], "col2": [6, 5, 8, 9, 10]})
+y = pd.Series([1, 2, 3, 4, 5])
 
-y = pd.Series([1, 1, 0, 0, 1])
-k = 4
-result = []
-for idx in range(X_test.shape[0]):
-    distance = np.sqrt(
-        np.sum(X.apply(lambda X: (X - X_test.iloc[idx]) ** 2, axis=1), axis=1)
-    )
+y_test = y[2 < y]
+y_test.reset_index(drop=True)
+y_test.index += 1
 
-    print(distance)
-
-    min_ind = np.argpartition(distance, k)[:k]
-    print(min_ind)
-    class1 = np.sum(y[min_ind])
-    class0 = len(y) - class1
-    print(f"class1: {class1}")
-    print(f"class0: {class0}")
-    result.append(1) if class1 >= class0 else result.append(0)
-
-print(result)
+print(y_test)
+print(y_test.shape[0])

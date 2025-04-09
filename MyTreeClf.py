224df2629fa7ac07b692b631001d3a05f5d0d63c
Voenkomp <remesey97@gmail.com>
2025-04-09 17:19:53 +1000

    finished best metric function

--- MyTreeClf.py
+++ MyTreeClf.py
@@ -18,22 +18,26 @@ class MyTreeClf:
         return f"MyTreeClf class: max_depth={self.max_depth}, min_samples_split={self.min_samples_split}, max_leafs={self.max_leafs}"
 
     def get_entropy(self, group):
+        eps = 1e-15
         return -(
-            (group.sum() / group.shape[0]) * np.log2(group.sum() / group.shape[0])
+            (group.sum() / group.shape[0])
+            * np.log2((group.sum() + eps) / group.shape[0])
             + (
                 ((group.shape[0] - group.sum()) / group.shape[0])
-                * np.log2((group.shape[0] - group.sum()) / group.shape[0])
+                * np.log2(((group.shape[0] - group.sum()) + eps) / group.shape[0])
             )
         )
 
     def get_sum_entropy(self, elem, X, y, col):
         left_group, right_group = y[X[col] <= elem], y[X[col] > elem]
-        left_entropy = self.get_entropy(left_group)
-        right_entropy = self.get_entropy(right_group)
+        left_entropy = (left_group.shape[0] / X.shape[0]) * self.get_entropy(left_group)
+        right_entropy = (right_group.shape[0] / X.shape[0]) * self.get_entropy(
+            right_group
+        )
         return left_entropy + right_entropy
 
     def get_best_split(self, X: pd.DataFrame, y: pd.Series):
-        eps = 1e-15
+        best_split = ("", 0, 0)
         for col in X.columns:
             unique_splits = (
                 X[col].drop_duplicates().sort_values().rolling(window=2).mean()[1:]
@@ -46,10 +50,21 @@ class MyTreeClf:
             best_split_col = pd.DataFrame(
                 {
                     "un_spl": unique_splits,
-                    "entropy": S0
-                    - unique_splits.apply(self.get_sum_entropy, X, y, col),
+                    "ig": S0 - unique_splits.apply(self.get_sum_entropy, X, y, col),
                 }
             )
+            max_split_ind = best_split_col["ig"].max().index
+
+            if best_split_col["ig"].iloc[max_split_ind] > best_split[2]:
+                best_split = (col, best_split_col.iloc[max_split_ind].to_list())
+                best_split = (
+                    col,
+                    best_split_col[best_split_col["ig"] == best_split_col["ig"].max()][
+                        "un_spl"
+                    ],
+                    best_split_col["ig"].max(),
+                )
+        return best_split
 
 
 obj1 = MyTreeClf()
--- scratch.py
+++ scratch.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 
-x = pd.Series([1, 2, 3, 4, 5])
+x = pd.DataFrame({"col1": [1, 2, 3, 4, 5], "col2": [6, 7, 8, 9, 10]})
 y = pd.Series([6, 7, 8, 9, 10])
 
-print((x * y).sum())
-print(np.dot(x, y))
+print(x.idxmax(axis="index"))
